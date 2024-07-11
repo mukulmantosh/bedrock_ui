4ffe1d370f6459be6ea2cf4c76532d863f1255ca
Mukul Mantosh <mukulmantosh@yahoo.com>
2024-07-11 16:55:07 +0530

    Update WebSocket handling and UI in websocket-ai-model.jsx
    This commit modifies WebSocket message handling in websocket-ai-model.jsx, removing the unnecessary formatting of event data. The UI is also updated with added select options and a structured layout within the card components. The look of the send message button has also been tweaked for better visibility.

--- src/components/websocket-ai-model.jsx
+++ src/components/websocket-ai-model.jsx
@@ -15,8 +15,7 @@ function WebSocketComponent() {
         setWs(websocket);
 
         websocket.onmessage = (event) => {
-            const formatted = event.data.replace(/\s+/g, ' ').trim();
-            setMessage(prevState => prevState + formatted + "\n") // Append streaming text
+            setMessage(prevState => prevState + event.data + "\n") // Append streaming text
         }
         websocket.onopen = () => {
             setConnectionStatus({
@@ -55,47 +54,50 @@ function WebSocketComponent() {
             <div className="container">
                 <div className="columns">
                     <div className="column is-8">
-                         <textarea value={textValue}
-                                   onChange={(event) => setTextValue(event.target.value)}
-                                   className="textarea is-focused">
+                        <div className="card">
+                            <div className="card-content">
+                                <div className="select is-info mb-4">
+                                    <select>
+                                        <option>Amazon Titan Text G1 - Lite</option>
+                                        <option>Titan Text G1 - Express</option>
+                                        <option>Claude 3 Sonnet</option>
+                                        <option>Claude 3 Haiku</option>
+                                        <option>Llama3 8B</option>
+                                        <option>Llama3 70B</option>
+                                    </select>
+                                </div>
+                                <textarea value={textValue}
+                                          onChange={(event) => setTextValue(event.target.value)}
+                                          className="textarea is-focused">
                          </textarea>
-                        <div className="fixed-grid has-6-cols">
-                            <div className="grid">
-                                <div className="cell">
-                                    <button className="button is-primary mt-4" onClick={sendMessage}>
-                                        Send Message
-                                    </button>
-                                </div>
-                                <div className="cell is-col-span-4"></div>
-                                <div className="cell mt-4">
-                                    <GoDotFill className={connectionStatus?.label}/>
-                                    <span className={connectionStatus?.label}> {connectionStatus?.text}</span>
-                                </div>
 
+                                <div className="fixed-grid has-6-cols">
+                                    <div className="grid">
+                                        <div className="cell">
+                                            <button className="button is-primary is-medium mt-4 has-text-white"
+                                                    onClick={sendMessage}>
+                                                Send Message
+                                            </button>
+                                        </div>
+                                        <div className="cell is-col-span-4"></div>
+                                        <div className="cell mt-4">
+                                            <GoDotFill className={connectionStatus?.label}/>
+                                            <span className={connectionStatus?.label}> {connectionStatus?.text}</span>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div className="column is-three-quarters is-left">
-
-
-                        </div>
-
                     </div>
                     <div className="column is-8">
-                        <div className="card">
+                    <div className="card">
                             <div className="card-content">
                                 <ReactTyped className="subtitle has-text-grey typing-text" strings={[message]} typeSpeed={10}  />
                             </div>
                         </div>
-
                     </div>
-
-
-
-
                 </div>
             </div>
-
-
         </div>
     );
 
